cbab49ae44b06527c2d2e6b672e7e112e79f5013
Cuong <cuong@Cuongs-MacBook-Pro.local>
2023-06-13 22:37:07 +0700

    trial pop, push navigation

--- Navigation/Navigation/SubView1.swift
+++ Navigation/Navigation/SubView1.swift
@@ -1,5 +1,5 @@
 //
-//  StoryboardTableViewController.swift
+//  UISubView1.swift
 //  Navigation
 //
 //  Created by Cuong on 13/06/2023.
@@ -7,22 +7,23 @@
 
 import UIKit
 
-class StoryboardTableViewController: UIViewController {
-    
+class SubView1: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         initlayout()
     }
     
     func initlayout() {
-        self.navigationItem.title = "StoryboardTable ViewController"
-        
+        self.navigationItem.title = "SubView 1"
     }
-
     
-    @IBAction func moveToSubView1(_ sender: Any) {
+    @IBAction func moveToSubView2(_ sender: Any) {
         let st = UIStoryboard.init(name: "Main", bundle: nil)
-        let vc = st.instantiateViewController(withIdentifier: "SubView1") as! SubView1
+        let vc = st.instantiateViewController(withIdentifier: "SubView2") as! SubView2
         self.navigationController?.pushViewController(vc, animated: true)
     }
+    
+
 }
